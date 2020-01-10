a248abad984c5a79dc05576d0cf7899c2979637f
Hrvojka Štrtak <hrvojka.strtak@gmail.com>
2020-01-10 23:20:44 +0100

    corrected

--- zadatak_2.py
+++ zadatak_2.py
@@ -3,11 +3,11 @@ znamenka_2 = int(input("Unesi drugi (pozitivan jednoznamenkasti) broj: "))
 broj_1 = (int(str(znamenka_1) + str(znamenka_2)))
 broj_2 = (int(str(znamenka_2) + str(znamenka_1)))
 
-if broj_1 % 2 ==0 and broj_2 % 2 !=0:
+if broj_1 % 2 == 0 and broj_2 % 2 != 0:
     print(f"Parni brojevi stvoreni od znamenaka {znamenka_1} i {znamenka_2} su: {broj_1}.")
-elif broj_1 % 2 !=0 and broj_2 % 2 ==0:
+elif broj_1 % 2 != 0 and broj_2 % 2 == 0:
     print(f"Parni brojevi stvoreni od znamenaka {znamenka_1} i {znamenka_2} su: {broj_2}.")
-elif broj_1 % 2 ==0 and broj_2 % 2 ==0:
+elif broj_1 % 2 == 0 and broj_2 % 2 == 0:
     print(f"Parni brojevi stvoreni od znamenaka {znamenka_1} i {znamenka_2} su: {broj_1} i {broj_2}.")
 else:
     print("Nema parnih brojeva.")
